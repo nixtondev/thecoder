2008bf958d8e808054ae4f11f3b8216f140cd3fa
~$ Root <83667327+nixtondev@users.noreply.github.com>
2022-10-25 19:53:16 +0200

    The coder

---
+++ matrix.sh
@@ -0,0 +1,253 @@
+#!/bin/bash
+
+# Add colors options & font text options
+export ITALIC='\e[3m'
+export B='\e[1m'
+export DIM='\e[2m'
+export UNDERLINE='\e[4m'
+export BLINK='\e[5m'
+export RESET_ALL='\e[0m'
+export HIDDEN='\e[7m' #(useful for passwords & invert the foreground and background colors)
+
+export COLOR_BLACK='\e[0;30m'
+export COLOR_GRAY='\e[1;30m'
+export COLOR_RED='\e[0;31m'
+export COLOR_LIGHT_RED='\e[1;31m'
+export COLOR_GREEN='\e[0;32m'
+export COLOR_LIGHT_GREEN='\e[1;32m'
+export COLOR_BROWN='\e[0;33m'
+export COLOR_YELLOW='\e[1;33m'
+export COLOR_BLUE='\e[0;34m'
+export COLOR_LIGHT_BLUE='\e[1;34m'
+export COLOR_PURPLE='\e[0;35m'
+export COLOR_LIGHT_PURPLE='\e[1;35m'
+export COLOR_CYAN='\e[0;36m'
+export COLOR_LIGHT_CYAN='\e[1;36m'
+export COLOR_LIGHT_GRAY='\e[0;37m'
+export COLOR_WHITE='\e[1;37m'
+
+export V='Version: 0.5.2'
+
+#Wake up, Neo...
+/usr/bin/clear
+
+echo -e ${COLOR_LIGHT_GREEN}${B}
+echo -e "W"
+sleep 1
+/usr/bin/clear
+echo -e "Wa"
+sleep 1
+/usr/bin/clear
+echo -e "Wak"
+sleep 1
+/usr/bin/clear
+echo -e "Wake"
+sleep 1
+/usr/bin/clear
+echo -e "Wake u"
+sleep 1
+/usr/bin/clear
+echo -e "Wake up"
+sleep 1
+/usr/bin/clear
+echo -e "Wake up,"
+sleep 1
+/usr/bin/clear
+echo -e "Wake up, N"
+sleep 1
+/usr/bin/clear
+echo -e "Wake up, Ne"
+sleep 1
+/usr/bin/clear
+echo -e "Wake up, Neo"
+sleep 1
+/usr/bin/clear
+echo -e "Wake up, Neo."
+sleep 1
+/usr/bin/clear
+echo -e "Wake up, Neo.."
+sleep 1
+/usr/bin/clear
+echo -e "Wake up, Neo..."
+sleep 5
+
+/usr/bin/clear
+
+#The matrix has you...
+
+echo -e "T"
+sleep 1
+/usr/bin/clear
+echo -e "Th"
+sleep 1
+/usr/bin/clear
+echo -e "The"
+sleep 1
+/usr/bin/clear
+echo -e "The m"
+sleep 1
+/usr/bin/clear
+echo -e "The ma"
+sleep 1
+/usr/bin/clear
+echo -e "The mat"
+sleep 1
+/usr/bin/clear
+echo -e "The matr"
+sleep 1
+/usr/bin/clear
+echo -e "The matri"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix h"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix ha"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix has"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix has y"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix has yo"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix has you"
+sleep 1
+/usr/bin/clear
+echo -e "The matrix has you."
+sleep 1
+/usr/bin/clear
+echo -e "The matrix has you.."
+sleep 1
+/usr/bin/clear
+echo -e "The matrix has you..."
+sleep 5
+
+/usr/bin/clear
+
+#Follow the white rabbit.
+echo -e "F"
+sleep 1
+/usr/bin/clear
+echo -e "Fo"
+sleep 1
+/usr/bin/clear
+echo -e "Fol"
+sleep 1
+/usr/bin/clear
+echo -e "Foll"
+sleep 1
+/usr/bin/clear
+echo -e "Follo"
+sleep 1
+/usr/bin/clear
+echo -e "Follow"
+sleep 1
+/usr/bin/clear
+echo -e "Follow t"
+sleep 1
+/usr/bin/clear
+echo -e "Follow th"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the w"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the wh"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the whi"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the whit"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white r"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white ra"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white rab"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white rabb"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white rabbi"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white rabbit"
+sleep 1
+/usr/bin/clear
+echo -e "Follow the white rabbit."
+sleep 5
+
+/usr/bin/clear
+#Knock, Knock, Neo.
+echo -e "K"
+sleep 1
+/usr/bin/clear
+echo -e "Kn"
+sleep 1
+/usr/bin/clear
+echo -e "Kno"
+sleep 1
+/usr/bin/clear
+echo -e "Knoc"
+sleep 1
+/usr/bin/clear
+echo -e "Knock"
+sleep 1
+/usr/bin/clear
+echo -e "Knock,"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, K"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Kn"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Kno"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knoc"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knoc"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knock"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knock,"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knock, N"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knock, Ne"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knock, Neo"
+sleep 1
+/usr/bin/clear
+echo -e "Knock, Knock, Neo."
+sleep 5
+echo -e ${RESET_ALL}
+sudo apt-get install cmatrix >/dev/null 2>&1 && sleep 1
+
+cmatrix
